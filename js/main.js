52f05ba0aa9cf728e76c0782829a3e7e57204f8d
Pemildebra <pemildebra@gmail.com>
2023-06-10 20:58:50 -0500

    fotos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-cont photoContainer = document.getElementById(photo-container);
+var photoContainer = document.getElementById("photo-container");
+
+content = '<div class="column">';
+for(let i=0; i< 20; i++){
+    if(i!=0 && (i % 5 == 0)){
+        content += `
+        </div><div class="column">
+        ${getPhotoElement(i+1)}`;
+    }else{
+        content += getPhotoElement(i+1);
+    }
+}
+
+content += "</div>";
+
+photoContainer.innerHTML = content;
+
+function getPhotoElement(photo_id){
+    return `<a href="#" class="photo">
+        <img loading="lazy" alt="Photo ${photo_id}" src="img/${photo_id}.jpg" />
+    </a>`;
+}
